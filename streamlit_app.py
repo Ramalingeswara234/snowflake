3514d16d43cd57f9d7cc0ca76ede1cc6f072fd57
Ramalingeswara234 <vrama234@gmail.com>
2022-07-10 17:46:59 +0530

    Update streamlit_app.py
    Added Breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit;
 
 streamlit.title('my new diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blue Berry Oat Meal')
+streamlit.text('OATs Dosa')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Rango Egg')
