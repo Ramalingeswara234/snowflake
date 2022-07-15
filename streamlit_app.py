fa0e8727a9a1d46a7f05243497bb1f4469c53e6b
Ramalingeswara234 <vrama234@gmail.com>
2022-07-16 00:12:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit;
 import pandas;
 #import snowflake.connector;
+import requests;
+
 
 streamlit.title('my new diner')
 streamlit.header('Breakfast Menu')
@@ -15,3 +17,6 @@ fruits_selected = streamlit.multiselect('Pick some Fruits', list(my_fruit_list.i
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
